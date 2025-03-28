22d121c7bcc9b090d379d909760676fd32a5b18c
Ikechukwu <iyke497@gmail.com>
2025-03-28 14:02:26 +0100

    curl commands to retrieve json data

---
+++ pull.sh
@@ -0,0 +1,2 @@
+curl -X GET "https://api.eyemark.ng/api/surveys/f0a3d43b-f7ee-4417-a338-7ba33ab9da14/responses/?limit=25" \
+  -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzUwODU1NTc0LCJpYXQiOjE3NDMwNzk1NzQsImp0aSI6IjBhNDg3ZWI5YmVlZjQwYTBiMjIyMmQ1Yjc1ZGQ0MDZiIiwidXNlcl9pZCI6NjI4fQ.Lsxg__plY4Z7GHN1uJ9X5yNCSW3VJnGI75I57WjwyRk"
